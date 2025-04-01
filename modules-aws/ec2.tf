22df381ed63ce29f43d17506f8c506ea76d931ac
Ravan cardoso <ravanreis@gmail.com>
2025-03-31 21:37:28 -0300

    update module ec2

--- modules-aws/ec2.tf
+++ modules-aws/ec2.tf
@@ -3,12 +3,12 @@ resource "aws_key_pair" "key" {
   public_key = file("./aws-key.pub")
 }
 
-resource "aws_instance" "vm" {
-  ami                         = "ami-0d1ddd83282187d18"
+resource "aws_instance" "ec2" {
+  ami                         = "ami-04f167a56786e4b09"
   instance_type               = "t3.nano"
   key_name                    = aws_key_pair.key.key_name
-  subnet_id                   = module.vpc.subnet_id  
-  vpc_security_group_ids      = [module.vpc.security_group_id]  
+  subnet_id                   = module.vpc.subnet_id
+  vpc_security_group_ids      = [module.vpc.security_group_id]
   associate_public_ip_address = true
 
   provisioner "local-exec" {
