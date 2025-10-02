e2764574672270c4c5b5bc9acee7054689500a39
Ravan cardoso <ravanreis@gmail.com>
2025-10-02 13:19:35 -0300

    create module S3 e rds

--- modules-aws/aws/SDLC/s3.tf
+++ modules-aws/aws/SDLC/s3.tf
@@ -1,4 +1,19 @@
-resource "aws_s3_bucket" "bucket" {
+# ---------------------------------------------------------------------------------------------------------------------
+# AMAZON S3
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Allows configuring public access block settings at the account level for Amazon S3.
+resource "aws_s3_account_public_access_block" "block_account_public_access" {
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+
+#bucket state terraform
+
+resource "aws_s3_bucket" "state-terraform" {
   bucket = var.bucket_name
   acl    = "private"
 
@@ -20,5 +35,5 @@ variable "bucket_name" {
 variable "environment" {
   description = "The environment for tagging"
   type        = string
-  default     = "dev"
+  default     = "terraform"
 }
